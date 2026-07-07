31cb246afc544a79d88b1c8a24145ebc534050b3
Stevy Endaman <stevyabessolo@gmail.com>
2026-07-07 07:02:34 -0400

    chore: add pint, phpstan and rector config

---
+++ rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+//        __DIR__.'/config',
+        __DIR__.'/src',
+//        __DIR__.'/routes',
+//        __DIR__.'/tests',
+//        __DIR__.'/resources',
+    ])
+    ->withSets([
+
+    ])
+    ->withTypeCoverageLevel(8)
+    ->withCodeQualityLevel(6)
+    ->withPreparedSets(
+        deadCode: true,
+        earlyReturn: true,
+    );
