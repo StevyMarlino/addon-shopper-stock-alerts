eff4c00f119d07167e3aa22b4578dc43021dfaa1
Stevy Endaman <stevyabessolo@gmail.com>
2026-07-08 16:27:39 -0400

    chore: rector update

--- rector.php
+++ rector.php
@@ -6,11 +6,11 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-//        __DIR__.'/config',
+        //        __DIR__.'/config',
         __DIR__.'/src',
-//        __DIR__.'/routes',
-//        __DIR__.'/tests',
-//        __DIR__.'/resources',
+        //        __DIR__.'/routes',
+        //        __DIR__.'/tests',
+        //        __DIR__.'/resources',
     ])
     ->withSets([
 
